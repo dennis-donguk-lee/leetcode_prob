1e962d0c15cace5977506ee93baad61c216adff4
Dennis-Donguk <dennis.donguk.lee@gmail.com>
2020-09-03 18:58:37 -0700

    solution

--- reorder_log_files/reorder_log_files.cpp
+++ reorder_log_files/reorder_log_files.cpp
@@ -1,31 +1,32 @@
-#include <string>         // std::string
-
 class Solution {
 public:
+    bool isDigitLead(string str) {
+        int idx = str.find(' ');
+        
+        return isdigit(str[idx+1]);
+    }
+    
     vector<string> reorderLogFiles(vector<string>& logs) {
-        vector<string> ressult;
-        vector<string> letVec;
-        vector<string> digVec;
-
-        // loop thru logs
-        for (auto&& log : logs)
-        {
-            // find 1st space (string::find(' '))
-            std::size_t found = 
-
-            // determine whether let or dig
-
-            // if dig
-                // check identifier to sort
-                    // push back to dig vec
-
-            // if let
-                // sort in lexicographical manner (std::lexicographical_compare(foo,foo+5,bar,bar+9))
-                    // push back to let vec
+        vector<string> digit;
+        vector<string> alpha;
+        
+        for(auto s : logs) {
+           if(isDigitLead(s)) 
+               digit.push_back(s);
+           else
+               alpha.push_back(s) ;   
         }
-
-        // attach dig vec to let vec and assign to result
-
-        // return result
+        
+        sort(alpha.begin(), alpha.end(), [](string a, string b)
+             {
+                 int id1 = a.find(' ');
+                 int id2 = b.find(' ');
+                 
+                 return a.substr(id1 + 1) <= b.substr(id2+1);
+             });
+        
+        alpha.insert(alpha.end(), digit.begin(), digit.end());
+        
+        return alpha;
     }
 };
